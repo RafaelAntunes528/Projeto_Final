f0ad9e709950890cac9af425716e6a237ffc3e4c
RafaelAntunes528 <rafa13ant@gmail.com>
2022-10-06 14:16:13 +0100

    Next.js pronto para uso

---
+++ projeto-fj/pages/api/contas/index.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function handler(req, res) {
+    if (req.method === "GET") {
+        
+    } else {
+        return res.status(404)
+    }
+
+}
