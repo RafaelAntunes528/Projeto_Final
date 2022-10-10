d0ac97bed2fba0850df93c31867fdbd01363904d
RafaelAntunes528 <rafa13ant@gmail.com>
2022-10-10 15:16:09 +0100

    Login e Registo

--- projeto-fj/pages/homePage.jsx
+++ projeto-fj/pages/homePage.jsx
@@ -9,7 +9,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Login></Login>
+      <HomePage></HomePage>
     </div>
   )
 }
