a6f651eb07929fdc977b4dbee0318445387bb1cb
RafaelAntunes528 <rafa13ant@gmail.com>
2022-10-11 14:17:48 +0100

    anhdjsadas

--- projeto-fj/src/components/pageOffersFL.jsx
+++ projeto-fj/src/components/pageOffersFL.jsx
@@ -2,12 +2,26 @@ import SideBar from "./sideBar";
 import styles from '../../styles/pageOffersFL.module.css'
 export default function OffersFL (){
     return(
-        <div className={styles.mae}>
-            <div classname={styles.side}>
-                <SideBar></SideBar> 
+        <div>
+            <div>
+                <SideBar></SideBar>
             </div>
             <div className={styles.main}>
-                <h1>TESTE</h1>
+                <div className={styles.q1}>
+                    <div className={styles.q2}>
+                        <p className={styles.title1}>As suas propostas de trabalho</p>
+                        <div className={styles.icone}>
+                            <AddCircleOutlineIcon />
+                        </div>
+
+                    </div>
+                    <div className={styles.q3}>
+                        <p className={styles.title2}>Não tem propostas de trabalho publicadas</p>
+                        <p>Clique no botão <AddCircleOutlineIcon /> para criar uma oferta de trabalho!</p>
+                    </div>
+
+
+                </div>
             </div>
         </div>
     )
