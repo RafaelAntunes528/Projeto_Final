5d7f051903c050d15e61284f9b897bf1b835046f
Vasco Tavares <vascoramalho26@gmail.com>
2022-10-11 13:40:16 +0100

    a

--- projeto-fj/pages/index.jsx
+++ projeto-fj/pages/index.jsx
@@ -6,12 +6,13 @@ import NavBar from '../src/components/navBar'
 import Registro from '../src/components/registro'
 import styles from '../styles/Home.module.css'
 import SideBar from '../src/components/sideBar'
+import PageOffers from '../src/components/pageOffersCT'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HomePage></HomePage>
+      <PageOffers></PageOffers>
     </div>
   )
 }
---
+++ projeto-fj/src/components/pageOffersCT.jsx
@@ -0,0 +1,35 @@
+import styles from "../../styles/PageOffersCT.module.css"
+import SideBar from "./sideBar"
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+export default function PageOffers() {
+    return (
+
+        <div>
+            <div>
+                <SideBar></SideBar>
+            </div>
+            <div className={styles.main}>
+                <div className={styles.q1}>
+                    <div className={styles.q2}>
+                        <p className={styles.title1}>As suas propostas de trabalho</p>
+                        <div className={styles.icone}>
+                            <AddCircleOutlineIcon />
+                        </div>
+
+                    </div>
+                    <div className={styles.q3}>
+                        <p className={styles.title2}>Não tem propostas de trabalho publicadas</p>
+                        <p>Clique no botão <AddCircleOutlineIcon /> para criar uma oferta de trabalho!</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+
+
+
+
+}
