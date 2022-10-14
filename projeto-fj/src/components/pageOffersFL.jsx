f877d693f6b4bf2c44aaa2c0032931da30165031
Rafa2v88 <r.rodrigues_g@hotmail.com>
2022-10-14 13:43:35 +0100

    gfdydyhftd

--- projeto-fj/src/components/pageOffersFL.jsx
+++ projeto-fj/src/components/pageOffersFL.jsx
@@ -12,28 +12,29 @@ export default function OffersFL() {
             <div className={styles.main}>
                 <div className={styles.q1}>
                     <div className={styles.q2}>
-                        <div className={styles.icone1}>
-                            <SearchIcon></SearchIcon>
-                        </div>
+
                         <p className={styles.title1}>As suas propostas de trabalho</p>
                         <div className={styles.icone}>
                             <input type={"search"}></input>
+                            <div className={styles.icone1}>
+                                <SearchIcon></SearchIcon>
+                            </div>
                         </div>
+
 
 
                     </div>
                     <div className={styles.q5}>
                         <p className={styles.filtrar}>Filtrar por:</p>
-                        
-                        <div className={styles.regiao}><input type="checkbox"></input>
-                        
-                        <label for="scales">Região</label></div>
-                        <div className={styles.horario}><input type="checkbox"></input>
-                        
-                        <label for="scales">Horário</label></div>
-                        <div className={styles.pago}><input type="checkbox"></input>
-                        
-                        <label for="scales">Valor pago</label></div>
+
+                        <div className={styles.acertar}><input className={styles.chek} type="checkbox"></input>
+                            <label for="scales">Região</label>
+                        </div>
+                        <div className={styles.acertar}><input className={styles.chek} type="checkbox"></input>
+                            <label for="scales">Horário</label></div>
+                        <div className={styles.acertar} ><input className={styles.chek} type="checkbox"></input>
+
+                            <label for="scales">Valor</label></div>
 
                     </div>
 
